c803feb8584abe84d2eaa34a08253e7aab1cf096
Leandro Carrasco <carrascoleandro@gmail.com>
2021-09-18 01:13:12 -0300

    Version inicial del proyecto

---
+++ src/test/java/com/leacar21/technical/config/service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leacar21.technical.config.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
